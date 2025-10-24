a245a2331eb99d726d8febf6cf6e3bfa6cf5c58d
HammadRana1 <rana.ha@northeastern.edu>
2025-10-24 19:29:14 -0400

    Creating classes for the faculty
    Creating classes for the faculty

--- Group_Project_INFO5100/src/Business/Profiles/FacultyDirectory.java
+++ Group_Project_INFO5100/src/Business/Profiles/FacultyDirectory.java
@@ -19,6 +19,8 @@ public class FacultyDirectory {
     
   Business business;
  
+    Business business;
+ 
     ArrayList<FacultyProfile> facultylist;
     public FacultyDirectory(Business d) {
         business = d;
