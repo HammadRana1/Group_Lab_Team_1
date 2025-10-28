8edb00ed17464d62ef446afb820f94841a032b19
HammadRana1 <rana.ha@northeastern.edu>
2025-10-28 01:38:15 -0400

    Business University Catalouge
    Business University Catalouge

--- Group_Project_INFO5100/src/Business/University/CourseCatalog/Course.java
+++ Group_Project_INFO5100/src/Business/University/CourseCatalog/Course.java
@@ -9,5 +9,27 @@ package Business.University.CourseCatalog;
  * @author Hammad
  */
 public class Course {
-    
+    // ADD ALL OF THIS BELOW
+    String number;
+    String name;
+    int credits;
+    int price = 1500;
+
+    public Course(String numb, String n, int ch) {
+        number = numb;  // Course number first
+        name = n;       // Course name second
+        credits = ch;
+    }
+
+    public String getCourseNumber() {
+        return number;
+    }
+
+    public int getCoursePrice() {
+        return price * credits;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
 }
