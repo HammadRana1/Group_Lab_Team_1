cd4d012ebad9939d46bb62f1ca65c3ffd86f5b4d
HammadRana1 <rana.ha@northeastern.edu>
2025-10-27 22:12:27 -0400

    degree

--- Group_Project_INFO5100/src/Business/University/Degree/Degree.java
+++ Group_Project_INFO5100/src/Business/University/Degree/Degree.java
@@ -4,12 +4,12 @@
  */
 package Business.University.Degree;
 
-import Business.University.CourseCatalog.Course;  
+import Business.Person.Person;
+import Business.University.CourseSchedule.CourseLoad;
 import Business.University.CourseSchedule.SeatAssignment;  
-import Business.Profiles.StudentProfile;  
-
-import java.util.ArrayList;
-
+import Business.University.CourseCatalog.Course;
+import Business.Profiles.StudentProfile;
+import java.util.ArrayList; 
 
 /**
  *
@@ -20,58 +20,38 @@ public class Degree {
     String title;
     ArrayList<Course> corelist;
     ArrayList<Course> electives;
-
+    
     public Degree(String name) {
         title = name;
-        corelist = new ArrayList();
-        electives = new ArrayList();
+        corelist = new ArrayList<>();  // ✓ Added <>
+        electives = new ArrayList<>();  // ✓ Added <>
     }
-
+    
     public void addCoreCourse(Course c) {
         corelist.add(c);
-
     }
-
+    
     public void addElectiveCourse(Course c) {
         electives.add(c);
-
     }
-
+    
     public boolean isStudentReadyToGraduate(StudentProfile sp) {
-
-        //Extract transcript from studentprofile
-        //Extract the list of courses taken so far from the student transcript
-        //For each core course in the core list of the degree do the following:
-        //Check if the core class at hand is in the transcrip
-        //Repeat this check for the electives as well
-        ArrayList sas = sp.getCourseList(); //seatAssignments extracted from course loads
-
+        ArrayList<SeatAssignment> sas = sp.getCourseList(); // ✓ Fixed: added type parameter
         if (validateCoreClasses(sas) == false) {
             return false;
-        }//core classes satisfied
-
-        //Do the electives check in case some taken classes are not part of the electives
-      
-        //Check for the total number of credit hours that it is above 32
-
-        else return true; //student has at least 32 credit hours per NEU requirements
-
+        }
+        return true;
     }
-
+    
     public boolean validateCoreClasses(ArrayList<SeatAssignment> sas) {
-
-        //For each core course in the core list of the degree do the following:
-        //Check if the core class at hand is in the transcrip
-        //Repeat this check for the electives as well
         for (Course c : corelist) {
-            if (!isCoreSatisfied(sas, c)) { //if selected core class not on the trascript return false
+            if (!isCoreSatisfied(sas, c)) {
                 return false;
             }
         }
-        return true; //all core classes are on the transcript satifying core reqs
-
+        return true;
     }
-
+    
     public boolean isCoreSatisfied(ArrayList<SeatAssignment> sas, Course c) {
         for (SeatAssignment sa : sas) {
             if (sa.getAssociatedCourse().equals(c)) {
@@ -79,9 +59,8 @@ public class Degree {
             }
         }
         return false;
-
     }
-
+    
     public int getTotalElectiveCoursesTaken(ArrayList<SeatAssignment> sas) {
         int electivecount = 0;
         for (SeatAssignment sa : sas) {
@@ -90,9 +69,8 @@ public class Degree {
             }
         }
         return electivecount;
-
     }
-
+    
     public boolean isElectiveSatisfied(SeatAssignment sa) {
         for (Course c : electives) {
             if (sa.getAssociatedCourse().equals(c)) {
@@ -100,7 +78,7 @@ public class Degree {
             }
         }
         return false;
-
     }
+    
     
 }
